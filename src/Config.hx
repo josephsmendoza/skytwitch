3367554387bade98347fe78a6247111c708fddb9
josephsmendoza <josephsmendoza@gmail.com>
2021-07-25 04:37:26 +0000

    Document config options and include defaults

--- src/Config.hx
+++ src/Config.hx
@@ -1,12 +1,27 @@
-typedef Config = {
-    ssl:Bool,
-    host:String,
-    port:Int,
-    game:String,
-    pass:String,
-    user:String,
-    channel:String,
-    givePointTime:Int,
-    commands:Map<String, Int>,
-    users:Map<String, Int>
+class Config {
+    // use ssl to connect
+    public var ssl=false;
+    // hostname to connect to
+    public var host="irc.chat.twitch.tv";
+    // port to connect to
+    public var port=6667;
+    // game to send commands to
+    public var game="Skyrim";
+    // auth token
+    public var pass="oath:token";
+    // bot username
+    public var user="username";
+    // channel to monitor
+    public var channel="username";
+    // time in seconds to give a point
+    public var givePointTime=0;
+    // list of commands and their costs
+    public var commands= new Map<String, Int>();
+    // list of users and their points
+    public var users=new Map<String, Int>();
+    // default command cost
+    public var defaultCost=0;
+    public function new() {
+        //why
+    }
 }
